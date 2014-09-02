e3365e0340cc6b09ea8a1ea58f3fcfc7b16b3c9f
Ubuntu <ubuntu@ip-172-31-29-9.ap-southeast-2.compute.internal>
2014-09-02 13:27:37 +0000

    partB Search Menu

---
+++ assign1/partB/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	</head>
+	
+	<body>	<center>
+		<form action="results.php" method="GET">
+		<h1>Input Wine Name:</h1><input type="text" name="search" size="50"/>
+		<br/>
+		<input type="submit" value="Search"/>	
+		</form>		
+		</center>
+		
+	</body>
+
+</html>
